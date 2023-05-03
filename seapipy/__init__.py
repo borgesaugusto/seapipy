f3e01ac3c821552f3defe78e19773183acfb14d5
Augusto <borges.augustoar@gmail.com>
2023-05-03 13:56:54 +0200

    Teste failure solved

--- seapipy/__init__.py
+++ seapipy/__init__.py
@@ -1,6 +1,6 @@
 from . import lattice_class
 from . import surface_evolver
 from . import command
-# from . import stats
+from . import stats
 from . import example_tissues
 # TODO: Add documentation to all functions
